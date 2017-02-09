527664eb88a16001f80bf48049edf245ebecdb9c
Chang ByoungGil <Gil@Changui-MacBook-Air.local>
2017-02-09 22:10:26 +0900

    Add form
    Final week_3

--- src/feed/views.py
+++ src/feed/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Article, Comment, HashTag
+from django.http import HttpResponseRedirect
+
+
 # Create your views here.
 def index(request):
     # GET @ POST
@@ -37,12 +40,33 @@ def index(request):
 
 
 def detail(request, article_id):
+    # GET & POST
     article = Article.objects.get(id=article_id)
+    # comment_list = Comment.object.filter(article__id=article_id)
+    # comment_list = article.article_comments.all()
     hashtag_list = HashTag.objects.all()
     ctx = {
         "article" : article,
+        # "comment_list" : comment_list,
         "hashtag_list" : hashtag_list,
     }
+
+    if request.method == "GET":
+        pass
+    elif request.method== "POST" :
+        username = request.POST.get("username")
+        content = request.POST.get("content")
+        Comment.objects.create(
+            article=article,
+            username=username,
+            content=content
+        )
+        # print(username)
+        # print(content)
+
+        return HttpResponseRedirect("/{}/".format(article_id))
+
+
     return render(request, "detail.html", ctx)
 
 # def about(request):
